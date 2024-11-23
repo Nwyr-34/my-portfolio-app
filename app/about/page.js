96a89bfd38a9157598397f76939b19da8f849142
ManuDev <vargas_manuel08@hotmail.com>
2024-11-23 16:45:47 -0500

    feat: add new page

---
+++ app/about/page.js
@@ -0,0 +1,24 @@
+"use client"
+import { title } from "@/components/primitives";
+import {Image} from "@nextui-org/image";
+import {Button} from "@nextui-org/react";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1 className={title( {color: "pink"} )}>About</h1>
+
+
+
+      <Image
+      width={300}
+      alt="NextUI hero Image"
+      src="https://nextui.org/images/hero-card-complete.jpeg"
+    />
+    <Button color="primary">
+      Button
+    </Button>
+    </div>
+  );
+}
+
